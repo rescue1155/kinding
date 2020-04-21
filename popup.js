45b76e5b03a8b2f49a19e6894b5dac495d92d741
Zack Alger <zackalger@gmail.com>
2020-04-21 16:47:50 -0600

    testing out links and css functionality. Don't worry, it's just a test :)

---
+++ popup.js
@@ -0,0 +1,6 @@
+
+//example
+// document.getElementById("image").addEventListener("click", () => {
+//     console.log("clicked");
+//     chrome.tabs.create({ url: "https://www.youtube.com/" });
+// });
