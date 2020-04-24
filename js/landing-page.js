451736bfd4a62c1ae9a6bc84bdf19fcad6e90356
Zack Alger <zackalger@gmail.com>
2020-04-24 12:28:05 -0600

    added time selection page, working on type selection page

---
+++ js/landing-page.js
@@ -0,0 +1,4 @@
+var break_time_button = document.getElementById("break-time-button");
+break_time_button.addEventListener("click", () => {
+    window.location.href = "/html/time-selection.html";
+});
