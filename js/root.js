b91901c7c894ce0096d4c830e318d2024f2dce26
leifdaniel <64107290+leifdaniel@users.noreply.github.com>
2020-05-26 23:16:24 -0600

    Update root.js

--- js/root.js
+++ js/root.js
@@ -1,5 +1,5 @@
 document.getElementById("go-button").addEventListener("click", () => {
-    window.location.href = "/html/challenges-page.html";
+    window.location.href = "/html/challenges.html";
 });
 
 document.getElementById("more-info").addEventListener("click", () => {
