4238c59eb41073cf9815f58e90ba833d5f0b8ec1
leifdaniel <64107290+leifdaniel@users.noreply.github.com>
2020-05-26 20:15:08 -0600

    Delete landing-page.js

--- js/landing-page.js
+++
@@ -1,4 +0,0 @@
-var break_time_button = document.getElementById("break-time-button");
-break_time_button.addEventListener("click", () => {
-    window.location.href = "/html/time-selection.html";
-});
