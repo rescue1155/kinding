d7c0f04b36bcd6ba76ea2203ca831f3d7b9f4e8b
leifdaniel <64107290+leifdaniel@users.noreply.github.com>
2020-05-28 15:34:06 -0600

    Update challenges.js

--- js/challenges.js
+++ js/challenges.js
@@ -17,30 +17,3 @@ document.getElementById("encourage-button").addEventListener("click", () => {
 document.getElementById("failure-button").addEventListener("click", () => {
     window.location.href = "/html/root.html";
 });
-
-
-var mainContainer = document.getElementById("display-data");
-
-
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'keyCLfpLr70tFjAiJ'}).base('appw7q9xOL7nliwUP');
-
-base('Challenges-table').select({
-    // Selecting the first 3 records in Grid view:
-    maxRecords: 3,
-    view: "Grid view"
-}).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-
-    records.forEach(function(record) {
-        console.log('Retrieved', record.get('Challenges-row'));
-    });
-
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
