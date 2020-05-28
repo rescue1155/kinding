8058dd1f4365b2bfcd57124eb35910f0aebe4a67
leifdaniel <64107290+leifdaniel@users.noreply.github.com>
2020-05-27 22:22:25 -0600

    Update challenges.js

--- js/challenges.js
+++ js/challenges.js
@@ -2,6 +2,18 @@ document.getElementById("back-icon").addEventListener("click", () => {
     window.location.href = "/html/root.html";
 });
 
-document.getElementById("back-icon").addEventListener("click", () => {
+document.getElementById("success-button").addEventListener("click", () => {
+    window.location.href = "/html/root.html";
+});
+
+document.getElementById("reminder-button").addEventListener("click", () => {
+    window.location.href = "/html/root.html";
+});
+
+document.getElementById("encourage-button").addEventListener("click", () => {
+    window.location.href = "/html/root.html";
+});
+
+document.getElementById("failure-button").addEventListener("click", () => {
     window.location.href = "/html/root.html";
 });
