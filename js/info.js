c68965f38689ae0094e0fadf0c93522590cd2de5
leifdaniel <64107290+leifdaniel@users.noreply.github.com>
2020-05-27 21:06:05 -0600

    Update info.js

--- js/info.js
+++ js/info.js
@@ -2,6 +2,6 @@ document.getElementById("go-button").addEventListener("click", () => {
     window.location.href = "/html/challenges.html";
 });
 
-document.getElementById("back-button").addEventListener("click", () => {
+document.getElementById("back-icon").addEventListener("click", () => {
     window.location.href = "/html/root.html";
 });
