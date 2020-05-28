fca4aff586d8c0f72d14b84446c474adec8e956b
leifdaniel <64107290+leifdaniel@users.noreply.github.com>
2020-05-27 18:05:36 -0600

    Create info.js

---
+++ js/info.js
@@ -0,0 +1,7 @@
+document.getElementById("go-button").addEventListener("click", () => {
+    window.location.href = "/html/challenges.html";
+});
+
+document.getElementById("back-button").addEventListener("click", () => {
+    window.location.href = "/html/root.html";
+});
